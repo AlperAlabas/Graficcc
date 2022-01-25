125541209219e8a3adb54d362afd1c034fd396ef
Alper Alabaş <alper.alabas@ogr.sakarya.edu.tr>
2022-01-25 17:11:27 +0300

    Finalized

---
+++ Grafik/Grafik/build/CMakeFiles/Deneme.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/glad.c" "CMakeFiles/Deneme.dir/src/glad.c.obj" "gcc" "CMakeFiles/Deneme.dir/src/glad.c.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui.cpp" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_demo.cpp" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_demo.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_demo.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_draw.cpp" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_draw.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_impl_glfw.cpp" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_impl_glfw.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_impl_opengl3.cpp" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_impl_opengl3.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_tables.cpp" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_tables.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_widgets.cpp" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/C_/Users/karaa/Desktop/Grafik/Dependencies/imgui/imgui_widgets.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/Camera.cpp" "CMakeFiles/Deneme.dir/src/Camera.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/Camera.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/FileLoader.cpp" "CMakeFiles/Deneme.dir/src/FileLoader.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/FileLoader.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/GuiControl.cpp" "CMakeFiles/Deneme.dir/src/GuiControl.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/GuiControl.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/Mesh2D.cpp" "CMakeFiles/Deneme.dir/src/Mesh2D.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/Mesh2D.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/Mesh3D.cpp" "CMakeFiles/Deneme.dir/src/Mesh3D.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/Mesh3D.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/MeshManager.cpp" "CMakeFiles/Deneme.dir/src/MeshManager.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/MeshManager.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/ShaderProgram.cpp" "CMakeFiles/Deneme.dir/src/ShaderProgram.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/ShaderProgram.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/Timer.cpp" "CMakeFiles/Deneme.dir/src/Timer.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/Timer.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/UtilityFuncitons.cpp" "CMakeFiles/Deneme.dir/src/UtilityFuncitons.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/UtilityFuncitons.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/VertexArrayObject.cpp" "CMakeFiles/Deneme.dir/src/VertexArrayObject.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/VertexArrayObject.cpp.obj.d"
+  "C:/Users/karaa/Desktop/Grafik/Projects/grafik_proje/src/main.cpp" "CMakeFiles/Deneme.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Deneme.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
